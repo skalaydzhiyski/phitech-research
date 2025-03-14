85decb5ddecebe052c4f247d433e418a3c529211
Spas Kalaydzhiyski <spas.kalaydzhiyski@santander.co.uk>
2025-03-14 19:12:34 +0000

    latest

--- helpers.py
+++ helpers.py
@@ -3,11 +3,22 @@ import pandas as pd
 import vectorbt as vbt
 
 
+def make_backtest_universe(instruments, col='close', dropna=False):
+    series = [i[col] for i in instruments.values()]
+    res = pd.concat(series, keys=instruments.keys(), names=["ticker"], axis=1)
+    res.ffill(inplace=True)
+    return res
+
+
 def load_instruments(base_path="../../phitech-data/01_raw"):
-    return {
-        fname.split("_")[0]: pd.read_csv(f"{base_path}/{fname}")
-        for fname in os.listdir(base_path)
-    }
+    res = {}
+    for fname in os.listdir(base_path):
+        df = pd.read_csv(f"{base_path}/{fname}")
+        df['timestamp'] = df['date'] + " " + df['time']
+        df = df.drop(columns=['date', 'time'])
+        df = df.set_index('timestamp')
+        res[fname.split("_")[0]] = df
+    return res
 
 
 def to_indicator(series):
