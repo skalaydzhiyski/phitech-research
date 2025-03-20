378e3b100f06988ab138537023da96d52fc7ff15
Spas Kalaydzhiyski <spas.kalaydzhiyski@santander.co.uk>
2025-03-20 17:40:06 +0000

    latest

--- helpers.py
+++ helpers.py
@@ -6,12 +6,19 @@ import vectorbtpro as vbt
 class SierraChartData(vbt.Data):
     @classmethod
     def _load_from_local(
-        cls, symbol, interval, base_data_path="../../phitech-data/01_raw"
+        cls,
+        symbol,
+        interval,
+        start=None,
+        end=None,
+        base_data_path="../../phitech-data/01_raw",
     ):
         res = pd.read_csv(f"{base_data_path}/{symbol}_{interval}.csv")
         res["timestamp"] = res["date"] + " " + res["time"]
+        res["timestamp"] = pd.to_datetime(res.timestamp)
+        res = res.reset_index(drop=True).set_index("timestamp")
         res = res.drop(columns=["date", "time"])
-        return res
+        return res[start:end]
 
     @classmethod
     def fetch_symbol(cls, symbol, interval, **kwargs):
